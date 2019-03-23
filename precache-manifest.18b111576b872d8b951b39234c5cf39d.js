99106a79cddf50b422f58e735a0d0d3105531aa6
Максим Калинин <maxim.kalinin.work@gmail.com>
2019-03-23 23:50:39 +0300

    Updates

---
+++ precache-manifest.18b111576b872d8b951b39234c5cf39d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cf9d181a3c1e249b8bc",
+    "url": "/flip-demo/static/css/main.380bfb43.chunk.css"
+  },
+  {
+    "revision": "6cf9d181a3c1e249b8bc",
+    "url": "/flip-demo/static/js/main.dcd7be98.chunk.js"
+  },
+  {
+    "revision": "aba8193ffe2999bc9b63",
+    "url": "/flip-demo/static/js/runtime~main.0ceef400.js"
+  },
+  {
+    "revision": "d0e7179c8fb4a099054d",
+    "url": "/flip-demo/static/js/2.6400f79f.chunk.js"
+  },
+  {
+    "revision": "fd2df8537d49e7f9568c3ce5ac380ed1",
+    "url": "/flip-demo/index.html"
+  }
+];
